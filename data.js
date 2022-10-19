118bee0120f01becaccedf1c37c13ac6312c6f0e
manuel130489 <74316133+manuel130489@users.noreply.github.com>
2022-10-19 14:43:26 -0500

    Initial commit

---
+++ data.js
@@ -0,0 +1,49 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-img_20221014_215507_00_merged_eduted",
+      "name": "IMG_20221014_215507_00_merged_eduted",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": 0.20508106879041854,
+        "pitch": 0.00882593638982243,
+        "fov": 1.2933824216075565
+      },
+      "linkHotspots": [],
+      "infoHotspots": [
+        {
+          "yaw": -2.703198682232525,
+          "pitch": 0.009981737223991871,
+          "title": "Title",
+          "text": "Text"
+        }
+      ]
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": true,
+    "viewControlButtons": false
+  }
+};
